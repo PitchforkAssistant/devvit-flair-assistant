6d956e9ef87ab8478efc1bf5db5d0fb648cfda10
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-07-28 17:25:23 +0300

    add tests for the new functions

--- tests/handlers.test.ts
+++ tests/handlers.test.ts
@@ -1,4 +1,5 @@
-import {validateCustomTimeformat, validateFlairConfig} from "../src/handlers.js";
+import {ERROR_INVALID_JSON, ERROR_INVALID_LOCALE, ERROR_INVALID_SCHEMA, ERROR_INVALID_TIMEZONE} from "../src/constants.js";
+import {validateCustomLocale, validateCustomTimeformat, validateCustomTimezone, validateFlairConfig} from "../src/handlers.js";
 import * as validTestConfig from "./validtestconfig.json";
 
 describe("validateFlairConfig", () => {
@@ -9,16 +10,52 @@ describe("validateFlairConfig", () => {
         expect(await validateFlairConfig({value: "[]", isEditing: false})).toBeUndefined();
     });
     test("validateFlairConfig should return a string for an empty config", async () => {
-        expect(await validateFlairConfig({value: "", isEditing: false})).toBeTypeOf("string");
+        expect(await validateFlairConfig({value: "", isEditing: false})).toEqual(ERROR_INVALID_JSON);
     });
     test("validateFlairConfig should return a string for invalid JSON", async () => {
-        expect(await validateFlairConfig({value: "][23[123;2'1]]2140-112)9(){}213.sd/3", isEditing: false})).toBeTypeOf("string");
+        expect(await validateFlairConfig({value: "][23[123;2'1]]2140-112)9(){}213.sd/3", isEditing: false})).toEqual(ERROR_INVALID_JSON);
     });
     test("validateFlairConfig should return a string for no templateId in config entry", async () => {
-        expect(await validateFlairConfig({value: "[{}]", isEditing: false})).toBeTypeOf("string");
+        expect(await validateFlairConfig({value: "[{}]", isEditing: false})).toEqual(ERROR_INVALID_SCHEMA);
     });
 });
 
 test("validateCustomTimeformat should return nothing for a valid timeformat", async () => {
     expect(await validateCustomTimeformat({value: "yyyy-MM-dd HH:mm:ss", isEditing: false})).toBeUndefined();
+});
+
+describe("validateCustomTimezone", () => {
+    test.each([
+        "+02:00",
+        "-07:00",
+        "+00:15",
+        "America/New_York",
+        "Europe/London",
+    ])("validateCustomTimezone(%s) should return undefined", async input => {
+        expect(await validateCustomTimezone({value: input, isEditing: false})).toBeUndefined();
+    });
+
+    test.each([
+        "00:00",
+        "0",
+    ])("validateCustomTimezone(%s) should return string", async input => {
+        expect(await validateCustomTimezone({value: input, isEditing: false})).toEqual(ERROR_INVALID_TIMEZONE);
+    });
+});
+
+describe("validateCustomLocale", () => {
+    test.each([
+        "enUS",
+        "enGB",
+        "DE",
+    ])("validateCustomLocale(%s) should return undefined", async input => {
+        expect(await validateCustomLocale({value: input, isEditing: false})).toBeUndefined();
+    });
+
+    test.each([
+        "_lib",
+        "aww",
+    ])("validateCustomLocale(%s) should return string", async input => {
+        expect(await validateCustomLocale({value: input, isEditing: false})).toEqual(ERROR_INVALID_LOCALE);
+    });
 });
--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,19 +1,23 @@
-import {domainFromUrlString, hasPlaceholders, safeTimeformat} from "../src/helpers.js";
-
-const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000; // Needed to make tests pass in different timezones.
+import {de, enUS} from "date-fns/locale";
+import {domainFromUrlString, getLocaleFromString, getTimeDeltaInSeconds, hasPlaceholders, isValidDate, safeTimeformat, toNumberOrDefault} from "../src/helpers.js";
 
 // Date object, timeformat, expected output.
 test.each([
-    [new Date(""), "", ""], // Invalid date with missing timeformat should return empty string.
-    [new Date(""), "yyyy-MM-dd HH-mm-ss", ""], // Invalid date with valid timeformat should return empty string.
-    [new Date("2021-01-01T00:00:00.000Z"), "", ""], // Valid date with missing timeformat should return empty string.
-    [new Date(1494634269000 + timezoneOffset), "", ""], // Timestamp with missing timeformat should return empty string.
-    [new Date(1494634269000 + timezoneOffset), "yyyy-MM-dd HH-mm-ss", "2017-05-13 00-11-09"], // Timestamp with valid timeformat should return known string.
-    [new Date("2017-05-13T00:11:09.000"), "yyyy-MM-dd HH-mm-ss", "2017-05-13 00-11-09"], // Valid known date with valid timeformat should return known string.
-    [new Date("2023-06-09T17:44:13.123"), "yyyy/'W'w hh-mm-ssa", "2023/W23 05-44-13PM"], // Valid known date with valid timeformat should return known string.
-    [new Date("2023-06-09T17:44:13.123"), "yyyy/'Q'Q HH-mm-ss.SSS", "2023/Q2 17-44-13.123"], // Valid known date with valid timeformat should return known string.
-])("safeTimeformat(%s, %s) -> %s", (date, timeformat, expected) => {
-    expect(safeTimeformat(date, timeformat)).toEqual(expected);
+    [new Date(""), "", "UTC", enUS, ""], // Invalid date with missing timeformat should return empty string.
+    [new Date(""), "yyyy-MM-dd HH-mm-ss", "UTC", enUS, ""], // Invalid date with valid timeformat should return empty string.
+    [new Date("2021-01-01T00:00:00.000Z"), "", "UTC", enUS, ""], // Valid date with missing timeformat should return empty string.
+    [new Date(1494634269000), "", "UTC", enUS, ""], // Timestamp with missing timeformat should return empty string.
+    [new Date(1494634269000), "yyyy-MM-dd HH-mm-ss", "UTC", enUS, "2017-05-13 00-11-09"], // Timestamp with valid timeformat should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "UTC", enUS, "2017-05-13 00-11-09"], // Valid known date with valid timeformat should return known string.
+    [new Date("2023-06-09T17:44:13.123Z"), "yyyy/'W'w hh-mm-ssa", "UTC", enUS, "2023/W23 05-44-13PM"], // Valid known date with valid timeformat should return known string.
+    [new Date("2023-06-09T17:44:13.123Z"), "yyyy/'Q'Q HH-mm-ss.SSS", "UTC", enUS, "2023/Q2 17-44-13.123"], // Valid known date with valid timeformat should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "+10:00", enUS, "2017-05-13 10-11-09"], // Valid known date with valid timeformat and different timezone should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "-07:00", enUS, "2017-05-12 17-11-09"], // Valid known date with valid timeformat and different timezone should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "+02:00", enUS, "2017-05-13 02-11-09"], // Valid known date with valid timeformat and different timezone should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "UTC", enUS, "2017-05-13 00-11-09"], // Valid known date with valid timeformat and different timezone should return known string.
+    [new Date("2017-05-13T00:11:09.000Z"), "yyyy-MM-dd HH-mm-ss", "", enUS, "2017-05-13 00-11-09"], // Valid known date with valid timeformat and no timezone should return known UTC string.
+])("safeTimeformat(%s, %s, %s, %s) -> %s", (date, timeformat, timezone, locale, expected) => {
+    expect(safeTimeformat(date, timeformat, timezone, locale)).toEqual(expected);
 });
 
 // URL, expected output.
@@ -42,4 +46,43 @@ test.each([
     expect(hasPlaceholders(input)).toEqual(expected);
 });
 
-// TODO: Tests for replacePlaceholders and maybe populateTemplate
+test.each([
+    ["21421", 10, 21421],
+    ["any text", 10, 10],
+    ["234.4", 10, 234.4],
+    ["-2", 10, -2],
+    ["43310d", 10, 10],
+])("toNumberOrDefault(%s, %s) -> %s", (input, fallback, expected) => {
+    expect(toNumberOrDefault(input, fallback)).toEqual(expected);
+});
+
+test.each([
+    ["enUS", enUS],
+    ["ENUS", enUS],
+    ["potato", undefined],
+    ["DE", de],
+])("getLocaleFromString(%s) -> %s", (input, expected) => {
+    expect(getLocaleFromString(input)).toEqual(expected);
+});
+
+test.each([
+    [new Date(40000), new Date(20000), 20],
+    [new Date(""), new Date(20000), Infinity],
+    [new Date(""), new Date(""), Infinity],
+    [new Date(20000), new Date(""), Infinity],
+    [new Date("2023-06-09T17:44:13.123Z"), new Date("2021-01-01T00:00:00.000Z"), 76873453.123],
+])("getTimeDeltaInSeconds(%s, %s) -> %s", (dateA, dateB, expected) => {
+    expect(getTimeDeltaInSeconds(dateA, dateB)).toEqual(expected);
+});
+
+test.each([
+    [new Date(1494634269000), true],
+    [new Date(40000), true],
+    [new Date(""), false],
+    [new Date("potato"), false],
+    [new Date("2023-06-09T17:44:13.123Z"), true],
+])("isValidDate(%s) -> %s", (date, expected) => {
+    expect(isValidDate(date)).toEqual(expected);
+});
+
+// TODO: Tests for replacePlaceholders, hasPerformedActions, and hasPerformedAction
