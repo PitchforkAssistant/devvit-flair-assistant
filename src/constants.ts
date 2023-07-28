8b8d0f3fa333033b57e5deddb5bbc1035b42a9f7
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-07-28 11:02:24 +0300

    fix default custom timeformat

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const DEFAULT_HEADER_TEMPLATE = "Hi {{author}}! Thanks for posting to /r/
 
 export const DEFAULT_FOOTER_TEMPLATE = "If you have questions about this, please [contact our mods via moderator mail](https://www.reddit.com/message/compose?to={{subreddit}}) rather than replying here. Thank you!";
 
-export const DEFAULT_CUSTOM_TIMEFORMAT = "yyyy-mm-dd hh-mm-ss";
+export const DEFAULT_CUSTOM_TIMEFORMAT = "yyyy-MM-dd hh-mm-ss";
 
 export const DEFAULT_ACTION_DEBOUNCE = 10;
 
