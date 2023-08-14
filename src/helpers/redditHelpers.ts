99173b44aa5511924414c44a86bb1a8248d50314
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-08-14 07:05:57 +0300

    make more hasPerformedAction props optional

--- src/helpers/redditHelpers.ts
+++ src/helpers/redditHelpers.ts
@@ -2,13 +2,13 @@ import {ModActionType, RedditAPIClient} from "@devvit/public-api";
 import {logError} from "./miscHelpers.js";
 import {getTimeDeltaInSeconds} from "./dateHelpers.js";
 
-export async function hasPerformedAction (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionType: ModActionType, cutoffSeconds: number, includeParent: boolean, moderatorName?: string): Promise<boolean> {
+export async function hasPerformedAction (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionType: ModActionType, cutoffSeconds?: number, includeParent?: boolean, moderatorName?: string): Promise<boolean> {
     const modLog = await reddit.getModerationLog({subredditName, moderatorId: moderatorName, type: actionType, limit: 100, pageSize: 100}).all().catch(e => {
         logError(`Failed to fetch ${actionType} log for ${subredditName} by ${moderatorName ?? ""}`, e);
         return [];
     });
     for (const modAction of modLog) {
-        if (getTimeDeltaInSeconds(new Date(), modAction.createdAt) < cutoffSeconds) {
+        if (!cutoffSeconds || getTimeDeltaInSeconds(new Date(), modAction.createdAt) < cutoffSeconds) {
             if (modAction.target?.id === actionTargetId) {
                 return true;
             } else if (includeParent && modAction.target?.permalink?.startsWith(`/r/${subredditName}/comments/${actionTargetId.substring(3)}/`)) {
@@ -19,7 +19,7 @@ export async function hasPerformedAction (reddit: RedditAPIClient, subredditName
     return false;
 }
 
-export async function hasPerformedActions (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionTypes: ModActionType[], cutoffSeconds: number, includeParent: boolean, moderatorName?: string): Promise<boolean> {
+export async function hasPerformedActions (reddit: RedditAPIClient, subredditName: string, actionTargetId: string, actionTypes: ModActionType[], cutoffSeconds?: number, includeParent?: boolean, moderatorName?: string): Promise<boolean> {
     const actionChecks = actionTypes.map(actionType => hasPerformedAction(reddit, subredditName, actionTargetId, actionType, cutoffSeconds, includeParent, moderatorName));
     const results = await Promise.all(actionChecks);
     return results.includes(true);
